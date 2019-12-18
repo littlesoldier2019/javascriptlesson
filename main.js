8a4a5273a1a4da69477fff19f680a165fee3ef0f
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2019-12-18 15:52:08 +0200

    addtional exercises

--- main.js
+++ main.js
@@ -413,3 +413,34 @@ console.log(itCompanies)
 
 //Ex24
 console.log(itCompanies.splice())
+
+
+//ADDITIONAL EXERCISES
+
+// let message = prompt("How old are you?")
+// alert(`You are ${message} years old`);
+
+// let name = prompt("What is your name?","Write your name")
+// alert(`Welcome ${name}`);
+
+// let nameOfJavaScript = prompt("What is the “official” name of JavaScript?", "")
+// if (nameOfJavaScript == "ECMAScript") {
+//     alert("Right!");
+// } else {
+//     alert("You don't know? “ECMAScript”!")
+// }
+
+// let inputNumber = prompt("Write a number", "");
+// if (inputNumber > 0) {
+//     alert("1");
+// } else if (inputNumber < 0) {
+//     alert("-1");
+// } else  
+//     alert("0");
+
+let result = (a + b < 4)? "Below": "Over";
+
+let message1 = (login == "Employee")? "Hello":
+(login == "Director")? "Greetings":
+(login == "")? "No login":
+"";
